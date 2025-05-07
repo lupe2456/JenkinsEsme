074ab2b693cf6a30be5fb1c370f4136169ad8d81
lupe2456 <lupegomez2456@gmail.com>
2025-05-07 11:06:02 -0600

    new branch presentation

--- src/main/java/com/example/jenkins_ci/helloController.java
+++ src/main/java/com/example/jenkins_ci/helloController.java
@@ -1,5 +1,6 @@
 package com.example.jenkins_ci;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class helloController {
     @GetMapping
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping
+    public String hello2(){
+        return "Hello World 2";
     }
 
 }
