6a36628b474803958480621714245f7e75b46b89
lupe2456 <lupegomez2456@gmail.com>
2025-05-13 10:57:07 -0600

    add hello3

--- src/main/java/com/example/jenkins_ci/helloController.java
+++ src/main/java/com/example/jenkins_ci/helloController.java
@@ -1,18 +1,23 @@
 package com.example.jenkins_ci;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class helloController {
 
-    @GetMapping
+    @GetMapping("/hello1")
     public String hello(){
         return "Hello World";
     }
 
-    @GetMapping
+    @PostMapping("/hello2")
     public String hello2(){
         return "Hello World 2 :)";
     }
 
+    @PostMapping("/hello3")
+    public String hello3(){
+        return "Hello World 3 :)";
+    }
 }
